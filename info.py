9bff4e07b90f2d5161a90f165c4c8edba6407332
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2025-10-06 17:14:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type T
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+xYZKRQc4IgphZTY9')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+Cgm91SVI5m1mODY1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/premuls')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mrperfectjikabot')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/cinemacompanysji')
 
