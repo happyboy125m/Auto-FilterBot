c01d08fec89f5f2f403f49abfef397cadd286bbe
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2025-10-06 17:09:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/f983d857f3c
 FSUB_IMG = (environ.get('FSUB_IMG', 'https://i.ibb.co/cShkPjcZ/x.jpg')).split() 
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1982405151').split()] 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002991955417').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002978685788').split()]
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003042648249'))  
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '  -1003042648249'))  
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002715836361'))  
