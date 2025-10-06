b4ce7a98beae57d1fde8d9b7b03c6ab57b51b92e
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2025-10-06 15:39:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002851154596') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gonubhai22103:gonubhai22103@cluster0.n7dgvx9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://challah7777_db_user:yIxEQLpKuwEvdH79@cluster0.rdwbzes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SilentXBotz_files')
 
