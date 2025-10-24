88e6fa28f9bcbc534038b22a069f838d70905434
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2025-10-24 23:04:09 +0530

    Change IS_VERIFY default value to False

--- info.py
+++ info.py
@@ -58,7 +58,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/cinemacompa
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1003170959167'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1003170959167'))
 
-IS_VERIFY = is_enabled('IS_VERIFY', Off)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002771812292'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002771812292'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
